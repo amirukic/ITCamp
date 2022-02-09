fbffb83c75b36cc1eaead08561e16975856a8b15
Amir <amir.ukic123@gmail.com>
2022-02-10 00:17:46 +0100

    object exercises 1

--- javascript/object/add.js
+++ javascript/object/add.js
@@ -5,8 +5,9 @@ let Eren = {
   calcBMI: function () {
     this.bmi = this.mass / (this.height * this.height);
     return this.bmi;
-  },
-};
+  }
+}
+
 let Zeke = {...Eren}
 Zeke.fullName = `Zeke Jeager`
 Zeke.mass = 78
@@ -14,3 +15,27 @@ Zeke.height = 1.78
 Eren.calcBMI()
 Zeke.calcBMI()
 console.log(Eren, Zeke);
+console.log(" ");
+if(Eren.bmi > Zeke.bmi){
+  console.log(`Veci BMI ima ${Eren.fullName} sa ${Eren.bmi}`);
+}
+else if(Zeke.bmi > Eren.bmi){
+  console.log(`Veci BMI ima ${Zeke.fullName} sa ${Zeke.bmi}`);
+}
+else{
+  console.log(`Oboje imaju isti BMI`);
+}
+
+
+///////////////////////////////////////////////////////////////////////////// DOMACI 1
+
+let student = {
+  name: "David Rayy",
+  sclass: "VI",
+  rollno: 12,
+};
+let niz = [];
+for (let el in student) {
+  niz.push(el);
+}
+console.log(niz);
