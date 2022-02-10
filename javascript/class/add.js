15c712cba53980cb73dc4b910c1fb80b5ca1e367
Amir <amir.ukic123@gmail.com>
2022-02-10 23:58:06 +0100

    class

---
+++ javascript/class/add.js
@@ -0,0 +1,51 @@
+// class covek {
+//     constructor(name,year,JMBG,work){
+//         this.name = name;
+//         this.year = year;
+//         this.JMBG = JMBG;
+//         this.work = work
+//     }
+//     radi(){
+//         return console.log(`Radi u ${this.work}`);
+//     }
+
+// }
+
+// covek1 = new covek("Amir", "20", "889718412","nigde")
+
+// console.log(covek1);
+
+//////////////////////////////////////////////////////////////////////////////////  VEZBA 1
+// o1 = {
+//     name: "Dzemil",
+//     surname: "Dupljak",
+// }
+// o2 = {}
+
+// let flatObj = Object.entries(o1)
+
+// for (let i = 0; i < flatObj.length; i++) {
+//     o2[flatObj[i][1]] = flatObj[i][0]
+// }
+// console.log(o1);
+// console.log("       ");
+
+// console.log(o2);
+
+/////////////////////////////////////////////////////////////////////////////////// VEZBA 2
+
+
+let covek1 = {
+    name: "Eren Jeager",
+    age: "23",
+    foundTitan: true
+}
+let covek2 = {
+    name: "Zeke Jeager",
+    age: "27",
+    foundTitan: false
+}
+
+function mymove() {
+    
+}
