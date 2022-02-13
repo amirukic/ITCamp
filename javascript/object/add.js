81bcd9bd2bb43c6d2c39fa28df9de725031b4546
Amir <amir.ukic123@gmail.com>
2022-02-14 00:03:29 +0100

    object

--- javascript/object/add.js
+++ javascript/object/add.js
@@ -1,30 +1,30 @@
-let Eren = {
-  fullName: `Eren Jeager`,
-  mass: 70,
-  height: 1.82,
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  }
-}
+// let Eren = {
+//   fullName: `Eren Jeager`,
+//   mass: 70,
+//   height: 1.82,
+//   calcBMI: function () {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   }
+// }
 
-let Zeke = {...Eren}
-Zeke.fullName = `Zeke Jeager`
-Zeke.mass = 78
-Zeke.height = 1.78
-Eren.calcBMI()
-Zeke.calcBMI()
-console.log(Eren, Zeke);
-console.log(" ");
-if(Eren.bmi > Zeke.bmi){
-  console.log(`Veci BMI ima ${Eren.fullName} sa ${Eren.bmi}`);
-}
-else if(Zeke.bmi > Eren.bmi){
-  console.log(`Veci BMI ima ${Zeke.fullName} sa ${Zeke.bmi}`);
-}
-else{
-  console.log(`Oboje imaju isti BMI`);
-}
+// let Zeke = {...Eren}
+// Zeke.fullName = `Zeke Jeager`
+// Zeke.mass = 78
+// Zeke.height = 1.78
+// Eren.calcBMI()
+// Zeke.calcBMI()
+// console.log(Eren, Zeke);
+// console.log(" ");
+// if(Eren.bmi > Zeke.bmi){
+//   console.log(`Veci BMI ima ${Eren.fullName} sa ${Eren.bmi}`);
+// }
+// else if(Zeke.bmi > Eren.bmi){
+//   console.log(`Veci BMI ima ${Zeke.fullName} sa ${Zeke.bmi}`);
+// }
+// else{
+//   console.log(`Oboje imaju isti BMI`);
+// }
 
 
 /////////////////////////////////////////////////////////////////////////////// DOMACI 1
@@ -39,31 +39,41 @@ else{
 //   niz.push(el);
 // }
 // console.log(niz);
-//////////////////////////////////////////////////////////////////////////////DOMACI 2
-var library = [
-  {
-    title: "Bill Gates",
-    author: "The Road Ahead",
-    readingStatus: true,
-  },
-  {
-    title: "Steve Jobs",
-    author: "Walter Isaacson",
-    readingStatus: true,
-  },
-  {
-    author: "Suzanne Collins",
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    readingStatus: false,
-  },
-];
+// //////////////////////////////////////////////////////////////////////////////DOMACI 2
+// var library = [
+//   {
+//     title: "Bill Gates",
+//     author: "The Road Ahead",
+//     readingStatus: true,
+//   },
+//   {
+//     title: "Steve Jobs",
+//     author: "Walter Isaacson",
+//     readingStatus: true,
+//   },
+//   {
+//     author: "Suzanne Collins",
+//     title: "Mockingjay: The Final Book of The Hunger Games",
+//     readingStatus: false,
+//   },
+// ];
 
-for (let i = 0; i < library.length; i++) {
-  if (library[i].readingStatus) {
-    console.log("Procitao " + library[i].title + " od " + library[i].author);
-  } else {
-    console.log(
-      "Nisam procitao " + library[i].title + " od " + library[i].author
-    );
-  }
-}
+// for (let i = 0; i < library.length; i++) {
+//   if (library[i].readingStatus) {
+//     console.log("Procitao " + library[i].title + " od " + library[i].author);
+//   } else {
+//     console.log(
+//       "Nisam procitao " + library[i].title + " od " + library[i].author
+//     );
+//   }
+// }
+
+
+
+// var student = {
+//   name : "David Rayy ",
+//   sclass : "VI",
+//   rollno : 12 
+// }
+// delete student.rollno
+// console.log(student);
