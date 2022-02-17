4b42175485052e9d80163f84f39adcf1712f323e
Amir <amir.ukic123@gmail.com>
2022-02-17 13:09:37 +0100

    class exercises 4

--- javascript/class/add.js
+++ javascript/class/add.js
@@ -1,20 +1,17 @@
 // class covek {
-//     constructor(name,year,JMBG,work){
-//         this.name = name;
-//         this.year = year;
-//         this.JMBG = JMBG;
-//         this.work = work
-//     }
-//     radi(){
-//         return console.log(`Radi u ${this.work}`);
-//     }
-
+//   constructor(name, year, JMBG, work) {
+//     this.name = name;
+//     this.year = year;
+//     this.JMBG = JMBG;
+//     this.work = work;
+//   }
+//   get radi() {
+//     return `Korisnik ${this.name} ima ${this.year}, njegov JMBG je ${this.JMBG} i radi u ${this.work}`
+//   }
 // }
+// covek1 = new covek("Amir", "20", "889718412", "rekic ");
 
-// covek1 = new covek("Amir", "20", "889718412","nigde")
-
-// console.log(covek1);
-
+// console.log(covek1.radi);
 //////////////////////////////////////////////////////////////////////////////////  VEZBA 1
 // o1 = {
 //     name: "Dzemil",
@@ -34,60 +31,111 @@
 
 /////////////////////////////////////////////////////////////////////////////////// VEZBA 2
 
-class Book {
-    constructor(title, genre, author) {
-      this.title = title;
-      this.genre = genre;
-      this.author = author;
-      this.genre = false;
-      this.read_date = null;
-    }
-  }
-  
-  class Booklist {
-    constructor() {
-      this.book_read = 0;
-      this.book_not_read = 0;
-      this.nextBook = null;
-      this.currBook = null;
-      this.lastBook = null;
-      this.bookShefl = [];
-    }
-  
-    add(book) {
-      if (book instanceof Book) {
-        this.bookShefl.push(book);
-      } else {
-        console.log("invalid");
-      }
-    }
-    finishCurrentBook() {
-      for (let i = 0; i < this.bookShelf.length; i++) {
-        const book = this.bookShefl[i];
-        if (book.title === this.currBook.title) {
-          this.bookShefl[i].read = true;
-          this.bookShefl[i].read_date = new Date().now();
-          this.lastBook = this.currBook;
-          this.currBook = this.nextBook;
-          if (this.bookShefl[i + 1]) {
-            this.nextBook = this.bookShefl[i + 1];
-          }
-          break;
-        }
-      }
-    }
-  }
-  
-  b1 = new Book("hp1", "magic", "jkr");
-  b2 = new Book("hp1", "magic", "jkr");
-  b3 = new Book("hp1", "magic", "jkr");
-  b4 = new Book("hp1", "magic", "jkr");
-  b5 = new Book("hp1", "magic", "jkr");
-  
-  booklist = new Booklist();
-  
-  booklist.add(b1);
-  booklist.add(b2);
-  booklist.add(b3);
-  booklist.add(b4);
-  booklist.add(b5);
+// class Book {
+//     constructor(title, genre, author) {
+//       this.title = title;
+//       this.genre = genre;
+//       this.author = author;
+//       this.genre = false;
+//       this.read_date = null;
+//     }
+//   }
+
+//   class Booklist {
+//     constructor() {
+//       this.book_read = 0;
+//       this.book_not_read = 0;
+//       this.nextBook = null;
+//       this.currBook = null;
+//       this.lastBook = null;
+//       this.bookShefl = [];
+//     }
+
+//     add(book) {
+//       if (book instanceof Book) {
+//         this.bookShefl.push(book);
+//       } else {
+//         console.log("invalid");
+//       }
+//     }
+//     finishCurrentBook() {
+//       for (let i = 0; i < this.bookShelf.length; i++) {
+//         const book = this.bookShefl[i];
+//         if (book.title === this.currBook.title) {
+//           this.bookShefl[i].read = true;
+//           this.bookShefl[i].read_date = new Date().now();
+//           this.lastBook = this.currBook;
+//           this.currBook = this.nextBook;
+//           if (this.bookShefl[i + 1]) {
+//             this.nextBook = this.bookShefl[i + 1];
+//           }
+//           break;
+//         }
+//       }
+//     }
+//   }
+
+//   b1 = new Book("hp1", "magic", "jkr");
+//   b2 = new Book("hp1", "magic", "jkr");
+//   b3 = new Book("hp1", "magic", "jkr");
+//   b4 = new Book("hp1", "magic", "jkr");
+//   b5 = new Book("hp1", "magic", "jkr");
+
+//   booklist = new Booklist();
+
+//   booklist.add(b1);
+//   booklist.add(b2);
+//   booklist.add(b3);
+//   booklist.add(b4);
+//   booklist.add(b5);
+
+// class Heroj {
+//   constructor() {
+//       this.health = 100;
+//   }
+//   get objaviStanje() {
+//       console.log("Health je " + this.health);
+//   }
+//   set primljenUdarac (udarac){
+//      if (this.health - udarac > 0){
+//        this.health -= udarac;
+//        this.objaviStanje;
+//     } else {
+//       console.log("Heroj je mrtav!");
+//     }
+//   }
+//   set ozdravljenje (pomoc){
+//     if (this.health + pomoc >100){
+//       this.health = 100;
+//       this.objaviStanje;
+//     } else {
+//       this.health += pomoc;
+//       this.objaviStanje;
+//     }
+//   }
+// }
+// var batman = new Heroj();
+// batman.objaviStanje
+// batman.primljenUdarac=50;
+// batman.ozdravljenje=20;
+// batman.primljenUdarac=80;
+///////////////////////////////////////////////////////////////////////////////////////////////////////ZAPREMINA CILINDRA
+
+// class cilindar {
+//   constructor(r, h) {
+//     this.r = r;
+//     this.h = h;
+//   }
+
+//   greska (){
+//     console.log(this.r === this.h);
+//   }
+//   get V() {
+//     return Math.PI * this.r * this.h;
+//   }
+// }
+// let cilindar1 = new cilindar(7, 4)
+// console.log(cilindar1.V);
+// cilindar1.greska()
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////VEZBA
+
