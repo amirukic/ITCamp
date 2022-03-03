8f4ebc35e76ad8d6ff66a3c8494219a0204e1b6e
Amir <amir.ukic123@gmail.com>
2022-03-03 17:43:09 +0100

    promises 5

--- javascript/async idknjs/add.js
+++ javascript/async idknjs/add.js
@@ -1,9 +1,60 @@
-function now() {
-  return 21;
+// function now() {
+//   return 21;
+// }
+// function later() {
+//   answer = answer * 2;
+//   console.log("Meaning of life:", answer);
+// }
+// var answer = now();
+// setTimeout(later, 1000);
+
+// const userLeft = false;
+// const userWatchingCatMeme = true;
+// function watchTutorialCallback(callback, errorCallback) {
+//   if (userLeft) {
+//     errorCallback({
+//       name: "User Left",
+//       message: ":(",
+//     });
+//   } else if (userWatchingCatMeme) {
+//     errorCallback({
+//       name: "User Watching Cat Meme",
+//       message: "WebDevSimplified < Cat",
+//     });
+//   } else {
+//     callback("Thumbs up and Subscribe");
+//   }
+// }
+// watchTutorialCallback(
+//   (message) => {
+//     console.log("Success:" + message);
+//   },
+//   (error) => {
+//     console.log(error.name + " " + error.message);
+//   }
+// );
+
+const userLeft = false;
+const userWatchingCatMeme = false;
+function prviPromis() {
+  return new Promise((resolve, reject) => {
+    if (userLeft) {
+      reject({
+        name: "User Left",
+        message: ":(",
+      });
+    } else if (userWatchingCatMeme) {
+      reject({
+        name: "User Watching Cat Meme",
+        message: "WebDevSimplified < Cat",
+      });
+    } else {
+      resolve("Thumbs up and Subscribe");
+    }
+  });
 }
-function later() {
-  answer = answer * 2;
-  console.log("Meaning of life:", answer);
-}
-var answer = now();
-setTimeout(later, 1000);
+prviPromis().then((message) => {
+  console.log('Succes:' + message);
+}).catch((error) => {
+  console.log(error.name + ' ' + error.message);
+})
