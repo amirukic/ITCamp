8aecdc284560d86b4d84e5dcaa78df828266bb65
Amir <amir.ukic123@gmail.com>
2022-07-15 19:55:16 +0200

    react

---
+++ mymove/src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./Card"
+
+const App = () => {
+  return (<div className="container" >
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+  </div>)
+}
+export default App;
---
+++ mymove/src/Card.jsx
@@ -0,0 +1,5 @@
+const Card = () => {
+    return (<div style={{border: "1px solid green", padding: "10px"}}>cao brate</div>)
+}
+
+export default Card;
