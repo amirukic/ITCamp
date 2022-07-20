7fa3fc249a7e6e7340ce4b62c62098cbb3f99da3
Amir <amir.ukic123@gmail.com>
2022-07-20 11:20:50 +0200

    TodoList

--- mymove/src/App.jsx
+++ mymove/src/App.jsx
@@ -1,13 +1,33 @@
-// import Card from "./Card"
-import "./App.css"
-import Counter from "./Counter"
-
+import { useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
 const App = () => {
-  return (<div className="main">
-    <Counter />
-  </div>
-  )
-}
+  const [data, setData] = useState([]);
 
+  let a = "";
+  return (
+    <div className="container">
+      <h1>TODO LIST</h1>
+      <div className="unos">
+        <input
+          type="text"
+          onChange={(e) => {
+            a = e.target.value;
+          }}
+        />
+        <button
+          onClick={() => {
+            setData([...data, a]);
+          }}
+        >
+          Submit
+        </button>
+      </div>
+      {data.map((el) => {
+        return <Card title={el} />;
+      })}
+    </div>
+  );
+};
 
 export default App;
